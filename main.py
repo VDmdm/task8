b94f94e3e76db72efaec8a701f3203a6733ea632
Dmitry <dmitrytob@gmail.com>
2024-01-25 17:08:26 +0300

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def test_function(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    print(test_function(1, 2))
---
+++ test.py
@@ -0,0 +1,11 @@
+from main import test_function as cal
+
+def test(a,b,c):
+    res = cal(a,b)
+
+    assert res == c
+
+
+test(10,20, 10 + 20)
+test(1,15, 1 + 15)
+test(40,150, 40 + 150)
